bfca86df7c40ab419e6538e2eba89774e0fcfdfe
Albert Wan <albert@alberts-air.dyndns.rice.edu>
2021-09-03 13:40:36 -0500

    added list

--- hello-angular/src/app/app.component.ts
+++ hello-angular/src/app/app.component.ts
@@ -10,16 +10,23 @@ export class AppComponent {
   public num1:number;
   public num2:number;
   public num3:number;
+  public arr_num:number[] = [];
   multiply(){
     this.num3=this.num1*this.num2;
+    //(this.arr_num).push(this.num3);
+    this.arr_num=[... this.arr_num, this.num3];
+    console.log(this.arr_num);
   }
   add(){
     this.num3=this.num1+this.num2;
+    //this.arr_num.push(this.num3);
   }
   subtract(){
     this.num3=this.num1-this.num2;
+    //this.arr_num.push(this.num3);
   }
   divide(){
     this.num3=this.num1/this.num2;
+    //this.arr_num.push(this.num3);
   }
 }
