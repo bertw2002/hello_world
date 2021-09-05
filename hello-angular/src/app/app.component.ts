d62b78004a0a0d97f58757f2a84231b9d934ad8e
Albert Wan <albert@alberts-air.dyndns.rice.edu>
2021-09-05 02:39:48 -0500

    M

--- hello-angular/src/app/app.component.ts
+++ hello-angular/src/app/app.component.ts
@@ -8,15 +8,58 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'hello-angular';
-  public arr1:number[]=[];
+  public arr1:string[]=[];
   public num1:number;
   public num2:number;
   public num3:number;
   public arr_num:number[] = [];
-  add0(x: number){
-    this.arr1=[... this.arr1, x]
+  add0(){
+    this.arr1=[... this.arr1, "0"];
   }
+  add1(){
+    this.arr1=[... this.arr1, "1"];
+  }
+  add2(){
+    this.arr1=[... this.arr1, "2"];
+  }
+  add3(){
+    this.arr1=[... this.arr1, "3"];
+  }
+  add4(){
+    this.arr1=[... this.arr1, "4"];
+  }
+  add5(){
+    this.arr1=[... this.arr1, "5"];
+  }
+  add6(){
+    this.arr1=[... this.arr1, "6"];
+  }
+  add7(){
+    this.arr1=[... this.arr1, "7"];
+  }
+  add8(){
+    this.arr1=[... this.arr1, "8"];
+  }
+  add9(){
+    this.arr1=[... this.arr1, "9"];
+  }
+  addSub(){
+    if (this.arr1.length!=0) {
+      if (this.arr1[-1]!="+" || this.arr1[-1]!="/" || this.arr1[-1]!="*") {
+        this.arr1=[... this.arr1, "-"];
+      }
 
+    }
+  }
+  addDiv(){
+    this.arr1=[... this.arr1, "/"];
+  }
+  addAdd(){
+    this.arr1=[... this.arr1, "+"];
+  }
+  addMult(){
+    this.arr1=[... this.arr1, "*"];
+  }
   multiply(){
     this.num3=this.num1*this.num2;
     //(this.arr_num).push(this.num3);
